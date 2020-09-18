d4cd7631ba644706772ec5ad141c433426bc0b67
BeiyuLinCS <beiyu.lin@wsu.edu>
2020-09-18 09:39:39 -0500

    updated

---
+++ teach/fall_2020/1370_labs/lab4/avgGradesSentinal.cpp
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////
+//
+// Name: <Put your name here>
+// Date: <Today's Date>
+// Class: <Your class number and section number, like: CSCI 1370.02>
+// Semester: <This semester, like: Spring 2012>
+// CSCI/CMPE 1370 Instructor: <Your lecture instructor's name>
+//
+// Use a sentinal controlled loop to find the average of grades
+// entered by the user. The sentinal value is -1.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int grade; // The grade entered by the user
+    int sum; // Sum of the grades
+    int count; // Number of grades entered
+    double avg; // Average of the grades
+    
+    
+    ////////////////////////////////
+    // Start of your code
+    
+    
+    // Loop until the user enters -1
+    // Each time through the loop, add the number input by the user
+    // to the sum. Make sure not to include the sentinal value in the
+    // sum or count!
+   
+    
+    
+    
+    
+    // Find the average of the grades entered by the user
+    // and print the result
+
+    
+    
+    
+    // End of your code
+    ////////////////////////////////
+
+    system("pause");
+
+    return 0;
+}
---
+++ teach/fall_2020/1370_labs/lab4/flag-while-input.cpp
@@ -0,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////
+//
+// Name: <Put your name here>
+// Date: <Today's date>
+// Class: <Your class number and section number, like: CSCI 1370.02>
+// Semester: <This semester, like: Spring 2012>
+// CSCI/CMPE 1370 Instructor: <Your lecture instructor's name>
+//
+// Use looping to ensure that the user inputs a proper value.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char ans; // The user's answer to the question
+    int age; // The user's age
+    bool correct; // The boolean flag
+
+    
+    
+    ////////////////////////////////
+    // Beginning of your code
+    
+    
+    // Ask the user a yes/no question and make sure the response is
+    // either y or n
+
+    
+    
+    
+    
+    
+    // Get the user's age
+    // Make sure the user enters her age as a number
+    
+    
+    
+    
+    
+
+    // End of your code
+    ////////////////////////////////
+    
+    cout << "Thanks for your input!" << endl;
+    
+
+    system("pause");
+    
+    return 0;
+}
