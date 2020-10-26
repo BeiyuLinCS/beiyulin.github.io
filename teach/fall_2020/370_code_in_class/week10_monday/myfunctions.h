48be854244dc23997709def304af30d2b2ecc3ca
BeiyuLinCS <beiyu.lin@wsu.edu>
2020-10-26 11:35:52 -0500

    updated

---
+++ teach/fall_2020/370_code_in_class/week10_monday/myfunctions.h
@@ -0,0 +1,11 @@
+// the header file of the c++ program. 
+#include <iostream>
+#include <string>
+#include <fstream> 
+using namespace std;
+const int LENGTHS = 10;
+
+// declare a function;
+// void array_func(int array_variable[], int length_array);
+void print_out_array(string name_array[], string position_array[], int kpi_array[], int len_array);
+
