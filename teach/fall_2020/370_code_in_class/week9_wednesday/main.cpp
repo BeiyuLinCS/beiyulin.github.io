18966e50e7faf5b10e38381b62dca03eac016e71
BeiyuLinCS <beiyu.lin@wsu.edu>
2020-10-26 08:37:17 -0500

    updated

--- teach/fall_2020/370_code_in_class/week9_wednesday/main.cpp
+++ teach/fall_2020/370_code_in_class/week9_wednesday/main.cpp
@@ -1,72 +1,83 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-/******** declare a function is always outside of the main function.  ********/
-int add_function(int arry[], int len_arr);
+const int MAX_ENTRIES = 200;
 
-/******** define a function  ********/
-int add_function(int arry[], int len_arr)
+void print_employee( string name[], string position[], 
+					 int review_score[], int i )
 {
-    for (int i = 0; i < len_arr; i++)
-    {
-
-        cout << arry[i] << endl;
-    }
-    return 0;
+	cout << "  " << name[i] << " (" 
+		 << position[i] << "): last review "
+		 << review_score[i] << endl;
 }
 
-/******** declare a function (allocate memory for a function) is always outside of the main function.  ********/
-int add_function1(int *input_var);
-
-/******** define a function (assign the value to a vraible)  ********/
-int add_function1(int &input_var)
+int lookup_employee( string name[], int empct, string lookup_name )
 {
-    // int results;
-    // cout << "the value of the input variable is " << input_var << endl;
-    input_var += input_var; // add itself twice
-    return input_var;
-}
-
-/**************************************** array and function **************************************************
-******** declare a function (allocate memory for a function) is always outside of the main function.  ********/
-int add_array_func(int input_var[], int len_array);
-
-/******** define a function (assign the value to a vraible)  ********/
-int add_array_func(int input_var[], int len_array)
-{   
-    for( int i = 0; i < len_array; i++)
-    {
-        input_var[i] += 10; // we will get input_var[0]: 15; input_var[1]: 16
-    }
-
-    // we will get input_var[0]: 15; input_var[1]: 16;
-    return input_var[0] + input_var[1];
+	// loop
+	for( int i=0;i<empct;i++ )
+	{
+		// check name against name array
+		if( lookup_name == name[i] )
+		{
+			return i;
+		}
+	}
+	return -1;
 }
 
 int main()
 {
-    int number;
-    number = 10;
+	ifstream shrug;
 
-    /******** call a function ********/
-    cout << "the returned value is " << add_function(number) << endl;
-    cout << "the value of number in main function is " << number << endl;
+	string name[MAX_ENTRIES];
+	string position[MAX_ENTRIES];
+	int review_score[MAX_ENTRIES];
 
+	int employee_ct = 0;
 
-    int array_var[2];
-    array_var[0] = 5;
-    array_var[1] = 6;
+	shrug.open( "2102-data.txt" );
 
-    for (int i = 0; i < 2; i++)
-    {
-        cout << "the value of i is " << i << " ";
-        array_var[i] = i + 5;
-        cout << "the value of array is " << array_var[i] << endl;
-    }
+	while( !shrug.eof() )
+	{
+		if( employee_ct < MAX_ENTRIES )
+		{
+			shrug >> name[employee_ct];
+			shrug >> position[employee_ct];
+			shrug >> review_score[employee_ct];
+			employee_ct++;
+	
+			shrug.ignore( 1, '\n' );
 
-    cout << "the return is " << add_array_func(array_var, 2) << endl;
-    cout << "the first element with the index 0 is " << array_var[0] << endl;
+		} else {
+			cout << "Bad things!" << endl;
+			return -1;
+		}
+	}
+
+	shrug.close();
+
+	cout << "We have " << employee_ct << " employees" << endl;
+
+	for( int i=0;i<employee_ct;i++ )
+	{
+		print_employee( name, position, review_score, i );
+	}
+
+	string lookup_name;
+	cout << "Please enter a name to lookup: ";
+	cin >> lookup_name;
+
+	int n = lookup_employee( name, employee_ct, lookup_name );
+	if( n != -1 )
+	{
+		print_employee( name, position, review_score, n );
+	}
+	else
+	{
+		cout << "That employee doesn't exist." << endl;
+	}
+
+	return 0;
 }
-
-
-
