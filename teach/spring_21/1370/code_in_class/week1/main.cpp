e8b79e66915c1e3647c2881fa478e7f5ceeffff3
BeiyuLinCS <beiyu.lin@wsu.edu>
2021-01-20 11:20:55 -0600

    updated

--- teach/spring_21/1370/code_in_class/week1/main.cpp
+++ teach/spring_21/1370/code_in_class/week1/main.cpp
@@ -7,6 +7,7 @@ int main()
     // cout: print things out on the terminal / console
     // endl: start a new line
     // one statment => compile first (to check if there is any error) => run => results
+    
     cout << 23+4 << endl;
     cout << 5-6*20 << endl;
     cout << 56.882 - (34/23) << endl;
@@ -16,15 +17,54 @@ int main()
     // anytime when we did the change, please save first. 
     // otherwise, it only compiles the things before the change. 
 
-    // define variables
-    int a, b, c, d; 
-    a = 3;
-    b = 4;
-    c = (a%b) *6;
-    d = c/b;
+    // declare variables
+    int a;
+    int b;
+    int c;
+    int d;
+    
+    // 3: letters/package
+    // variable a: a mailbox
+    // where can we put the mailbox
+    // how to allocate the memory for a variable <=> declare a variable
+    // a variable has three part:
+    // 1. the variable name: a
+    // 2. the location of the variable <=> address of a variable
+    // 3. the value that assigned to a variable. 
 
-    cout << endl;
+    a = 3; 
+    b = 4;
+
+    int i;
+    i = 10;
+
+    // 1. declare a variable in order to allocate memory for a variable.
+    float x;
+    // 2. assign some value to the variable. 
+    x = 2.5 + 3.0 * 2 - 6.4;
+
+    // declare a variable with character type
+    char char_variable;  // char_variable; charVariable
+    char_variable = 'a'; // assign a character 'a' to the variable
+
+    // declare a string variable named str_variable
+    string str_variable; // strVariable
+    str_variable = "today is Tuesday.";
+ 
+
+    // how to give the names of variables / identifiers
+    // 2 types of variables names:
+    // 1. connect words with _ to connect two words
+    // 2. captilize for the initial of the letter
+
+    c = (a % b) * 6 + 2 - 1;
+    d = c / b;
+
+    cout<< endl;
     cout << "the value of c " << c << endl;
     cout << "the value of d " << d << endl;
+
+    c = 100;
+    cout << "the updated value of c is " << c << endl;
 
 }
